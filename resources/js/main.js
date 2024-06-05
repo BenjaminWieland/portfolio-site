39c37b7455bf1debd8b46b93565c2c0d0b4f1905
BenWieland <131756375+BenjaminWieland@users.noreply.github.com>
2024-06-05 15:35:45 +0200

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,3 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.getElementById('burger');
+    const nav = document.getElementById('nav');
+
+    burger.addEventListener('click', function() {
+        nav.classList.toggle('nav-open');
+    });
+});
